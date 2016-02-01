aeaebb0c84b74f17734e751a0b1591bd3e90182b
Antoine GIRARD <sapk@users.noreply.github.com>
2016-02-01 22:36:56 +0100

    Change default name

--- www/ui/js/gast.js
+++ www/ui/js/gast.js
@@ -1,1 +1,1 @@
-var G = { version : '1.0.1', config : {"name":"GAST - Reggae Sun Ska","position":{"lat":"48.5632800","lng":"3.28355200"}} }
+var G = { version : '1.0.1', config : {"name":"PREUMS - Per Radio Emergency Units Management System","position":{"lat":"48.5632800","lng":"3.28355200"}} }
